50aecc3c2e7c0f73de8d3a3dbb1fb9bd883a2663
sahabaishakhi <baishakhi@front.live>
2019-01-14 20:19:53 +0530

    commit message

---
+++ src/magitdemo/core.clj
@@ -0,0 +1,6 @@
+(ns magitdemo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/magitdemo/core_test.clj
@@ -0,0 +1,7 @@
+(ns magitdemo.core-test
+  (:require [clojure.test :refer :all]
+            [magitdemo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
